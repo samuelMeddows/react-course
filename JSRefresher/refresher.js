fcc102c24d9447d1fb2d1c68d77d21a606f19e31
Samuel Meddows <samuel.j.meddows@live.com.au>
2021-08-19 11:36:09 +1000

    JS refresher code update

--- JSRefresher/refresher.js
+++ JSRefresher/refresher.js
@@ -4,7 +4,6 @@
     let changing = "changing variable";
     const static = "static variable";
 
-
 //##################
 //#!Arrow Functions
 //##################
@@ -35,7 +34,6 @@
     // Shothand for a simple function
     const multiply = number => number * 2;
     console.log(multiply(2))
-
 
 //#####################################
 //#!Exports and Imports - Modular Code
@@ -72,6 +70,7 @@
 //#!Understanding Classes
 //#######################
     //Classes are blueprints for objects
+    //Inheritance is compareable to prototypes
     class Person {
         name = 'Sam'            //property
         call = () => {'...'}    //method
@@ -87,9 +86,115 @@
     //class Person extends Master
 
     //Example
-    class Person {
-        constructor() {
-            this.name = 'Sam'
+    class Human {                    //class
+        constructor() {              //constructor
+            this.gender = 'male';
+        }
+        
+        printGender(){               //method
+            console.log(this.gender)
         }
     }
 
+    class Person extends Human{      //class inheritence
+        constructor() {              //constructor
+            super();                 //executes the parent constuctor
+            this.name = 'Samantha';
+            this.gender = "Female";
+        }
+
+        printMyName() {              //method
+            console.log(this.name);
+        }
+    }
+
+    const person = new person();
+    Person.printMyName();
+    Person.printGender();
+
+//################################################
+//#!Calsses, Properties & Methods (Modern Syntax)
+//################################################
+    //Properties are like "variables attached to classes/objects"
+        //ES6 
+        /*
+        Constructor (){
+            this.myProperty = 'Value'
+        }
+        */
+    //ES7
+        myProperty = 'Value';
+
+    //Methods are like "fiunctions attached to classes/objects"
+    //ES6
+        //myMethod () {'...'}
+    //ES7
+        myMethod = () => {'...'}
+    
+    //Example ES7 (next Gen JS)
+    class Human {
+        gender = 'male';
+
+        printGender = () => {
+            console.log(this.gender);
+        }
+    }
+
+    class Person extends Human {
+        name = 'Samantha';
+        gender = "female";
+
+        printMyName = () => {
+            console.log(this.name);
+        } 
+    }
+    const person = new person();
+    Person.printMyName();
+    Person.printGender();
+
+//#########################
+//#!Spread & Rest Operators
+//#########################
+    //Spread Operator
+    const numbers = [1, 2, 3];
+    const newNumbers = [numbers, 4]; //This would crreate [[numbers], 4]
+    const newNumbers = [...numbers, 4]; //Creates [1, 2, 3, 4].
+    console.log(newNumbers);
+
+    const person = {
+        name: 'Sam'
+    };
+
+    const newPerson = {
+        ...person,
+        age: 28
+    };
+    console.log(newPerson) 
+    /*
+    [object Object] {
+        ages: 28,
+        name: "Max"
+    }
+    */
+
+    //Rest Operator
+    const filter = (...args) => {
+        return args.filter(el => el == 1); //Inline is true statement tre/false
+    }
+    console.log(filter(1, 2, 3)); //Output [1]
+
+//#################
+//#!Deconsturcting
+//#################
+    //Array Deconstructing
+    const numbers = [1, 2, 3];
+    [num1, , num3] = [numbers];
+    console.log(num1, num3)
+
+    //Object Deconstructing
+    //{name} = {name: 'Sam', age: 20}
+    //console.log{name} //Output: Sam
+
+//#################
+//#!Reference and Primative Types
+//#################
