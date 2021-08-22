6e83c13be402fac6933beac133c5753cdd6b6660
Samuel Meddows <samuel.j.meddows@live.com.au>
2021-08-22 13:10:31 +1000

    Added React Project Files

---
+++ 01-starting-project/src/App.js
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <div>
+      <h1>My Tools</h1>
+      <div className='card'>
+        <h2>TITLE</h2>
+        <div className='actions'>
+          <botton className='btn'>Delete</botton>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
+
